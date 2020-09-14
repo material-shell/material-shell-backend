8d0f97558e5cb32fa7f550fd9730fbe121b06e02
PapyElGringo <adrienpeslerbe@gmail.com>
2020-09-15 00:01:42 +0200

    add log

--- database.js
+++ database.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const initDB = () => {
+  console.log(
+    "auth",
+    `mongodb+srv://materialShellBackend:${process.env.MONGODB_PASSWORD}@materialshell.talx9.mongodb.net/materialshell?retryWrites=true&w=majority`
+  );
   mongoose.connect(
     `mongodb+srv://materialShellBackend:${process.env.MONGODB_PASSWORD}@materialshell.talx9.mongodb.net/materialshell?retryWrites=true&w=majority`,
     {
